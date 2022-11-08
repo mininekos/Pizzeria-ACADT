f36dc870d8cd8f3eb8acaf07ce39d5d83040d8c9
mininekos <manuel2g2m@gmail.com>
2022-11-08 13:43:47 +0100

    V1.8

--- app/src/main/java/com/example/pizzeria/Enums/TipoIngredientes.java
+++ app/src/main/java/com/example/pizzeria/Enums/TipoIngredientes.java
@@ -2,8 +2,9 @@ package com.example.pizzeria.Enums;
 
 public enum TipoIngredientes {
 
-    LECHUGA(0,"Lechuga"),CEBOLLA(0,"Cebolla"),TOMATE(0,"Tomate"),MAIZ(0,"Maiz"),ACEITUNAS(0,"Aceitunas"),
-    COL(0,"Col"),QUESO_SUAVE(0.5,"Queso suave"),QUESO_FETA(0.5,"Queso feta"),EXTRA_CARNE(0.5,"Extra carne"),
+    LECHUGA(0,"Lechuga"),CEBOLLA(0,"Cebolla"),TOMATE(0,"Tomate"),MAIZ(0,"Maiz"),
+    ACEITUNAS(0,"Aceitunas"), COL(0,"Col lombarda"),QUESO_SUAVE(0.5,"Queso suave"),
+    QUESO_FETA(0.5,"Queso feta"),EXTRA_CARNE(0.5,"Extra carne"),
     PATATAS(0.5,"Patatas dentro"),HUEVO(0.5,"Huevo");
 
     private double precio;
