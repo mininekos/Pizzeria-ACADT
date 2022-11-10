6fad50b8efa578b49298586272d4d693f0eb86d8
Manuel Jesús Guerrero Merino <manuel2g2m@gmail.com>
2022-11-11 00:18:49 +0100

    v2.0

--- app/src/main/java/com/example/pizzeria/DAOS/DAOUsuario.java
+++ app/src/main/java/com/example/pizzeria/DAOS/DAOUsuario.java
@@ -1,4 +1,32 @@
 package com.example.pizzeria.DAOS;
 
+import com.example.pizzeria.Recursos.Usuario;
+
+import java.util.ArrayList;
+
 public class DAOUsuario {
+
+    private static ArrayList<Usuario> lista;
+    private static DAOUsuario dao;
+
+    public static DAOUsuario getInstance(){
+
+        if (dao==null)
+            dao=new DAOUsuario();
+        if(lista==null)
+            lista=new ArrayList<Usuario>();
+        return dao;
+    }
+
+    public static ArrayList<Usuario> getLista(){
+        lista.add(new Usuario("admin","admin","coreo1"));
+        lista.add(new Usuario("Manu","1234","coreo2"));
+        return lista;
+    }
+
+    public static Boolean agregarUsuario(Usuario usuario){
+        lista.add(usuario);
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/pizzeria/Recursos/Usuario.java
+++ app/src/main/java/com/example/pizzeria/Recursos/Usuario.java
@@ -42,6 +42,6 @@ public class Usuario {
 
     @Override
     public String toString() {
-        return "Usuario" + nombre + '\'';
+        return "Usuario\n" + nombre;
     }
 }
